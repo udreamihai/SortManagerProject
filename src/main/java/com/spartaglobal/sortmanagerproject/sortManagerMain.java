83468dc6eaad04eb10eda035cb76b2f37d50ebcd
mihai <udreamihai@gmail.com>
2022-01-31 15:19:00 +0000

    generated random array of ints and copied them

--- src/main/java/com/spartaglobal/sortmanagerproject/sortManagerMain.java
+++ src/main/java/com/spartaglobal/sortmanagerproject/sortManagerMain.java
@@ -1,4 +1,19 @@
 package com.spartaglobal.sortmanagerproject;
 
+import java.util.*;
+import java.util.stream.*;
+
 public class sortManagerMain {
+    public static void main(String[] args) {
+        int[]  intsArray = IntStream.generate(() -> new Random().nextInt(500)).limit(40).toArray();
+
+        int[] bubbleSortArray = new int[40];
+        int[] mergeSortArray = new int[40];
+        System.arraycopy(intsArray, 0, bubbleSortArray, 0, 40);
+        System.out.println("Array for bubble sort: " + Arrays.toString(bubbleSortArray));
+        System.arraycopy(intsArray, 0, mergeSortArray, 0, 40);
+        System.out.println("Array for bubble sort: " + Arrays.toString(mergeSortArray));
+    }
+
+
 }
