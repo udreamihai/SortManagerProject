d38df9fe1e1a9175b92a95341af1e109b184d749
mihai <udreamihai@gmail.com>
2022-02-03 14:18:53 +0000

    tweaks

--- src/main/java/com/spartaglobal/sortmanagerproject/junit/model/SortManagerMain.java
+++ src/main/java/com/spartaglobal/sortmanagerproject/junit/model/SortManagerMain.java
@@ -5,52 +5,80 @@ import com.spartaglobal.sortmanagerproject.junit.view.SortView;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.FileWriter;   // Import the FileWriter class
 import java.util.*;
 import java.util.stream.*;
 
 public class SortManagerMain {
     public static Logger logger = LogManager.getLogger("SortManager Logger");
     public static void main(String[] args) {
-        logger.info("SortManager started successfully.");
-        final int arraySize = castVariables.arraySize; //array size
-        int[] bubbleSortArray = new int[arraySize]; //used for bubble sort
-        int[] mergeSortArray = new int[arraySize]; //used for merge sort
-        try {
-            System.arraycopy(castVariables.intsArray, 0, bubbleSortArray, 0, arraySize); //populate bubble sort array
-            System.arraycopy(castVariables.intsArray, 0, mergeSortArray, 0, arraySize); //populate merge sort array
-            logger.info("Random array [intsArray] of integers initialized correctly.");
-        } catch (Exception e) {
-            logger.error("Random array [intsArray] of integers was not initialized.", e);
-        }
-        System.out.println("===========================");
-        System.out.println("Original array of integers:");
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < castVariables.intsArray.length; i++){
-            sb.append(castVariables.intsArray[i]);
-            sb.append(", ");
-        }
-        for (int j = 0; j < sb.length(); j++){
-            System.out.printf("=");
-        }
-        System.out.printf("\n");
-        System.out.println(Arrays.toString(bubbleSortArray));
+
+        while (true) {
+            logger.info("SortManager started successfully.");
+
+            final int arraySize = castVariables.arraySize; //array size
+            int[] bubbleSortArray = new int[arraySize]; //used for bubble sort
+            int[] mergeSortArray = new int[arraySize]; //used for merge sort
+            try {
+                System.arraycopy(castVariables.intsArray, 0, bubbleSortArray, 0, arraySize); //populate bubble sort array
+                System.arraycopy(castVariables.intsArray, 0, mergeSortArray, 0, arraySize); //populate merge sort array
+                logger.info("Random array [intsArray] of integers initialized correctly.");
+            } catch (Exception e) {
+                logger.error("Random array [intsArray] of integers was not initialized.", e);
+            }
+            System.out.println("===========================");
+            System.out.println("Original array of integers:");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < castVariables.intsArray.length; i++) {
+                sb.append(castVariables.intsArray[i]);
+                sb.append(", ");
+            }
+            for (int j = 0; j < sb.length(); j++) {
+                System.out.printf("=");
+            }
+            System.out.printf("\n");
+            System.out.println(Arrays.toString(bubbleSortArray));
 
 
-        SortView view = new SortView();
-        int desiredSortType = view.getDesiredSortMethod();
-        SortController controller = new SortController();
-        int[] result = controller.initiateSort(desiredSortType);
-        long startTime = System.nanoTime();
-        controller.initiateSort(desiredSortType);
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime);
-        System.out.printf("The sort method took %d nanoseconds to run and the result is:\n", duration);
-        for (int j = 0; j < sb.length(); j++){
-            System.out.printf("=");
-        }
-        System.out.printf("\n");
-        view.displayResults(result);
+            SortView view = new SortView();
+            int desiredSortType = view.getDesiredSortMethod();
+            SortController controller = new SortController();
+            int[] result = controller.initiateSort(desiredSortType);
+            long startTime = System.nanoTime();
+            controller.initiateSort(desiredSortType);
+            long endTime = System.nanoTime();
+            long duration = (endTime - startTime);
 
+            //generate filenames to save the timings
+            String fileName = "";
+            if (desiredSortType == 1){
+                fileName += "Bubble";
+            } else if (desiredSortType == 2){
+                fileName += "Merge";
+            }
+            fileName += "Timing.txt";
+
+            try {
+                FileWriter myWriter = new FileWriter(fileName, true);
+                myWriter.write(Long.toString(duration));
+                myWriter.write("\n");
+                myWriter.close();
+                logger.info("Successfully wrote timing to the file.");
+            } catch (IOException e) {
+                logger.error("Error writing timing to the file.", e);
+                //e.printStackTrace();
+            }
+
+            System.out.printf("The sort method took %d nanoseconds to run and the result is:\n", duration);
+            for (int j = 0; j < sb.length(); j++) {
+                System.out.printf("=");
+            }
+            System.out.printf("\n");
+            view.displayResults(result);
+
+        }
     }
 
     public class castVariables{
--- src/main/java/com/spartaglobal/sortmanagerproject/junit/view/SortView.java
+++ src/main/java/com/spartaglobal/sortmanagerproject/junit/view/SortView.java
@@ -5,13 +5,15 @@ import com.spartaglobal.sortmanagerproject.junit.model.SortManagerMain;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import static com.spartaglobal.sortmanagerproject.junit.model.SortManagerMain.*;
+
 public class SortView {
 
     public int getDesiredSortMethod() {
         enum validOptions{
             Bubble(1),
             Merge(2),
-            Quit(3);
+            Quit(5);
 
             private int id;
             validOptions(int id){
@@ -24,25 +26,35 @@ public class SortView {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("============================");
-            System.out.println("| SELECT THE TYPE OF SORT  |");
-            System.out.println("============================");
-            System.out.println("| Options:                 |");
-            System.out.println("|        1. Bubble Sort    |");
-            System.out.println("|        2. Merge Sort     |");
-            System.out.println("|        3.                |");
-            System.out.println("|        4. Exit           |");
-            System.out.println("============================");
+            //show the menu
+            System.out.println("=====================================");
+            System.out.println("| SELECT THE TYPE OF SORT           |");
+            System.out.println("=====================================");
+            System.out.println("| Options:                          |");
+            System.out.println("|        1. Bubble Sort             |");
+            System.out.println("|        2. Merge Sort              |");
+            System.out.println("|        3.                         |");
+            System.out.println("|        4. Compare timings         |");
+            System.out.println("|        5. Exit                    |");
+            System.out.println("=====================================");
             System.out.printf("Select option: ");
             int desiredSortType = scanner.nextInt();
-            if (desiredSortType == 4){
-                SortManagerMain.logger.info("User chose to exit.");
+            if (desiredSortType == 5){ //quit
+                logger.info("User chose to exit.");
+                System.out.printf("Quitting...");
                 System.exit(0);
             }
+            Boolean isValid = false;
             for (validOptions option : validOptions.values()) {
                 if (desiredSortType == option.id) {
+                    isValid = true;
                     return desiredSortType;
                 }
+
+            }
+            if(!isValid) {
+                System.out.printf("Please enter a valid option!\n");
+                logger.info("User tried to be funny.");
             }
 
 
@@ -52,9 +64,9 @@ public class SortView {
     public void displayResults(int[] result) {
         try {
             System.out.println(Arrays.toString(result));
-            SortManagerMain.logger.info("Result displayed correctly.");
+            logger.info("Result displayed correctly.");
         } catch (Exception e){
-            SortManagerMain.logger.error("Error displaying result.", e);
+            logger.error("Error displaying result.", e);
         }
     }
 
